93ad9deaf76fa92df9d629cf873252ae1ddf2d21
김선우 <seonwoo960000@toss.im>
2025-01-25 11:28:59 +0900

    Add panic handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+
+#![no_std]
 fn main() {
-    println!("Hello, world!");
+}
+
+use core::panic::PanicInfo;
+
+/// Defines the function that the compiler should invoke when a panic occurs
+/// standard lib usually provides this function, but as we are using no_std, let's define our own
+/// Returning `!` means that the function is returning `never` type
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> !{
+    loop {
+
+    }
 }
