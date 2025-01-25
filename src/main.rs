bbba6cff87794c78b024a944c3f071a4c559070a
김선우 <seonwoo960000@toss.im>
2025-01-25 14:45:14 +0900

    add config.toml

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
-#![no_std]
-#![no_main]
-/// We don't want to use the normal entry point
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
 use core::panic::PanicInfo;
 
-/// Defines the function that the compiler should invoke when a panic occurs
-/// standard lib usually provides this function, but as we are using no_std, let's define our own
-/// Returning `!` means that the function is returning `never` type
+/// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-/// To ensure Rust compiler doesn't change our function name
-/// extern "C" is used to tell the compiler that it should use the C calling convention for this  function(instead of the unspecified Rust calling convention)
-/// This entry point is not called by any function, but invoked directly by the OS or bootloader. So instead of returning, the entry point should invoke the `exit` system call
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
     loop {}
 }
